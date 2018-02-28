9edfc7ae4053dcdb4416417df0b884b457289d79
Grigoriy Larin <larin.grishan@yandex.ru>
2018-03-01 01:06:18 +0300

    Complete homework

--- project/src/main.c
+++ project/src/main.c
@@ -8,35 +8,20 @@
 
 #define TST_FOO_FIX     1
 #define TST_FOO_IMPL    2
-#define TST_MOD_IMPL    3
+#define TST_MOD_IMPL	3
 
-
-/* NOTE(stitaevskiy):
- * We use `atoi` function just for simplification and code reducing.
- * This function doesn't report conversation errors.
- * For safety program we recommend using `strtol` and its analogs.
- * (See `man atoi` and `man strtol` for more info).
- *
- * const char str_num[] = "1234";
- * char* end = NULL;
- * int val = (int) strtol(str_num, &end, 0);
- * if (end != '\0') {
- *     //ERROR
- * }
- *
- * */
 
 int main(int argc, const char** argv) {
     if (argc < 3) {
         return ERR_ARGS_COUNT;
     }
-
     int test_case = atoi(argv[1]);
-    const char* data;
-    data = argv[2];
-
+    const char* data = argv[2];
     switch (test_case) {
         case TST_FOO_FIX: {
+	    if (argc != 3) {
+		    return 255;
+	    }
             size_t res = custom_strlen(data);
             printf("%zu\n", res);
             break;
@@ -45,22 +30,15 @@ int main(int argc, const char** argv) {
             if (argc != 4) {
                 return ERR_ARGS_COUNT;
             }
-            /* Comment to prevent `unused parameter` error
             int base = atoi(data);
             int pow =  atoi(argv[3]);
-            int res = custom_pow(base, pow);    // TODO: Implement me
-
+            int res = custom_pow(base, pow);    // TODO(FireTiger)
             printf("%i\n", res);
-            */
             break;
         }
         case TST_MOD_IMPL: {
-            // Comment for prevent `unused variable` error
-            // int num = atoi(data);
-
-            // TODO: Print to stdout `1` if `num` is prime number and `0` otherwise
-            // This function MUST be implemented in
-            // a separate C-module (not in `main` or `utils` module)
+	    int n = atoi(data);
+            printf("%d", custom_is_simple(n));	 // TODO(FireTiger)
             break;
         }
         default: {
--- project/src/utils.c
+++ project/src/utils.c
@@ -1,14 +1,31 @@
 #include <string.h>
-
 #include "utils.h"
 
 size_t custom_strlen(const char* str) {
-    return sizeof(str);
+	unsigned strlen = 0;
+	while(str[strlen] != '\0') {
+		strlen++;
+	}
+	return strlen;
 }
 
-// TODO: Implement `power of` function
-/*
 int custom_pow(int base, int power) {
-    return 0;
+	if(base == 1 || power == 0)
+		return 1;
+	int i = 1;
+	long result = base;
+	for(; i < power; i++)
+		result *= base;
+	return result;
 }
-*/
+
+int custom_is_simple(int n) {
+	if(n < 2)
+		return 0;
+	int div = 1;
+	while(++div != n)
+		if(!(n % div))
+			return 0;
+	return 1;
+}
+// TODO(FireTiger): Implement `power of` function
